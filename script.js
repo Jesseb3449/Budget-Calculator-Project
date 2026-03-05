c5feee8b70b09a67fb5d5014a26f8a407e515577
Bigounce016 <elicreynolds928@gmail.com>
2026-03-05 09:39:16 -0800

    I worked on html and CSS

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 async function data() {
     const url = "https://eecu-data-server.vercel.app/data";
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 
 
 }
